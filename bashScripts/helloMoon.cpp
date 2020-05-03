83fbdbaf4080569334fdc1a0af3fc078db058c22
root <root@DESKTOP-KADVQCS.localdomain>
2020-05-03 18:09:23 +0200

    This is a executable bash file that sends 3 c++ programs to MOSS

---
+++ bashScripts/helloMoon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=6;
+    int j=7;
+    int n=j+i;
+    int z=i/2;
+    cout << "Hello Moon!";
+    return 0;
+}
---
+++ bashScripts/helloWorld1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=6;
+    int j=7;
+    int n=i+j;
+    cout << "Hello World!";
+    return 0;
+}
