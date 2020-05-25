541c97ffd3a4d91a0cb59c616299fb31b6990009
Mihai Tudor <tudorbrangapeicu@gmail.com>
2020-05-25 12:08:04 +0200

    Made them accesible through a directory

--- bashScripts/Send.php
+++ bashScripts/Send.php
@@ -1,7 +1,7 @@
 <?php
 
 	$old_path = getcwd();
-	chdir('Submissions');//ensures that code is done in specific directory of server
+	//chdir('Moss');//ensures that code is done in specific directory of server
 	$output = shell_exec('./sendbash.sh');
 	chdir($old_path);
 	$arr = explode("\n", $output);
