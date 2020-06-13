d0b1ed7c9a4868a2890c180a177b7b3ac794fc89
jwubites23 <44243777+jwubites23@users.noreply.github.com>
2020-06-13 17:41:12 +0200

    upload final

--- bashScripts/sendbash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./moss.pl -l $1 -d ./Submissions/*assignsubmission_file_/*.cpp
--- moss/plagiarism_form.php
+++ moss/plagiarism_form.php
@@ -11,11 +11,9 @@ class plagiarism_setup_form extends moodleform {
         $mform =& $this->_form;
         $choices = array('No','Yes');
         $mform->addElement('html', get_string('mossexplain', 'plagiarism_moss'));
-        $mform->addElement('checkbox', 'moss_use', get_string('usenew', 'plagiarism_moss'));
+        $mform->addElement('checkbox', 'moss_use', get_string('usemoss', 'plagiarism_moss'));
 
-        $mform->addElement('textarea', 'moss_student_disclosure', get_string('studentdisclosure','plagiarism_moss'),'wrap="virtual" rows="6" cols="50"');
-        $mform->addHelpButton('moss_student_disclosure', 'studentdisclosure', 'plagiarism_moss');
-        $mform->setDefault('moss_student_disclosure', get_string('studentdisclosuredefault','plagiarism_moss'));
+  
 
         $this->add_action_buttons(true);
     }
